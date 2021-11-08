7530a186c1a978a43db433d7dc1f7acee02ae654
Rob MacKinnon <rome@villagertech.com>
2021-11-08 13:02:06 -0800

    Project renamed to DOMObjects Renamed to avoid contflicting project name DictGroup: Key name management bug corrected Pypi: pyproject.toml added, and general project directory restructure.

---
+++ src/DOMObjects/leaf.py
@@ -0,0 +1,8 @@
+__author__ = "Rob MacKinnon <rome@villagertech.com>"
+__package__ = "DOMObjects"
+__name__ = "DOMObjects.leaf"
+
+
+class DOMLeaf(object):
+    def __init__(self):
+        raise NotImplemented()
